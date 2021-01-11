0acd5e612fc0c14c02fd19fd689401477d24288a
quangnt18 <quangnt.itc@gmail.com>
2021-01-11 12:34:18 +0700

    add Station images and modidified Header, router

--- src/router/index.js
+++ src/router/index.js
@@ -20,80 +20,95 @@ const routes = [
         path: 'station/:id',
         component: () => import( './../Views/HomTemplate/DetailStationPage' )
       }
+      ,
+      {
+        path: 'airports',
+        component: () => import( './../Views/HomTemplate/AirportPage' )
+      }
+      ,
+      {
+        path: 'airports/:id',
+        component: () => import( './../Views/HomTemplate/DetailAirportPage' )
+      }
     ]
   },
   {
     path: "/admin",
     redirect: "/admin/dashboard",
   },
-  
+
   {
     path: "/admin",
-    component: () => import("./../Views/AdminTemplate"),
-     beforeEnter(to, from, next) {
-      if (localStorage.getItem("token")) {
-        try {
-          const user = jwtDecode(localStorage.getItem("token"));
-          if (user.userType === "Admin") {
+    component: () => import( "./../Views/AdminTemplate" ),
+    beforeEnter ( to, from, next )
+    {
+      if ( localStorage.getItem( "token" ) )
+      {
+        try
+        {
+          const user = jwtDecode( localStorage.getItem( "token" ) );
+          if ( user.userType === "Admin" )
+          {
             next();
           }
         } catch {
-          next("/admin/login");
+          next( "/admin/login" );
         }
-      } else {
-        next("/admin/login");
+      } else
+      {
+        next( "/admin/login" );
       }
     },
     children: [
       {
         path: "/admin/dashboard",
-        component: () => import("./../Views/AdminTemplate/DashboardPage"),
+        component: () => import( "./../Views/AdminTemplate/DashboardPage" ),
       },
       {
         path: "/admin/stations",
-        component: () => import("./../Views/AdminTemplate/StationPage"),
+        component: () => import( "./../Views/AdminTemplate/StationPage" ),
       },
       {
         path: "/admin/stations/:id/edit",
-        component: () => import("./../Views/AdminTemplate/EditStationPage"),
+        component: () => import( "./../Views/AdminTemplate/EditStationPage" ),
       },
       {
         path: "/admin/tickets",
-        component: () => import("./../Views/AdminTemplate/TicketPage"),
+        component: () => import( "./../Views/AdminTemplate/TicketPage" ),
       },
       {
         path: "/admin/airports",
-        component: () => import("./../Views/AdminTemplate/AirportPage"),
+        component: () => import( "./../Views/AdminTemplate/AirportPage" ),
       },
       {
         path: "/admin/airports/:id/edit",
-        component: () => import("./../Views/AdminTemplate/EditAirportPage"),
+        component: () => import( "./../Views/AdminTemplate/EditAirportPage" ),
       },
       {
         path: "/admin/airports/add",
-        component: () => import("./../Views/AdminTemplate/AddAirportPage"),
+        component: () => import( "./../Views/AdminTemplate/AddAirportPage" ),
       },
       {
         path: "/admin/accounts",
-        component: () => import("./../Views/AdminTemplate/AccountPage"),
+        component: () => import( "./../Views/AdminTemplate/AccountPage" ),
       },
       {
         path: "/admin/accounts/add",
-        component: () => import("./../Views/AdminTemplate/AddAccountPage"),
+        component: () => import( "./../Views/AdminTemplate/AddAccountPage" ),
       },
       {
         path: "/admin/accounts/update",
-        component: () => import("./../Views/AdminTemplate/EditAccountPage"),
+        component: () => import( "./../Views/AdminTemplate/EditAccountPage" ),
       },
       {
         path: "/admin/about",
-        component: () => import("./../Views/AdminTemplate/AboutPage"),
+        component: () => import( "./../Views/AdminTemplate/AboutPage" ),
       },
     ],
   },
   {
     path: "/admin/login",
-    component: () => import("./../Views/AdminTemplate/AuthPage"),
+    component: () => import( "./../Views/AdminTemplate/AuthPage" ),
   },
   {
     path: '/:patchMatch(.*)*',
--- src/store/airport/index.js
+++ src/store/airport/index.js
@@ -9,17 +9,20 @@ const state = {
 };
 
 const mutations = {
-  [types.M_AIRPORT_REQUEST](state) {
+  [ types.M_AIRPORT_REQUEST ] ( state )
+  {
     state.loading = true;
     state.data = null;
     state.err = null;
   },
-  [types.M_AIRPORT_SUCCESS](state, payload) {
+  [ types.M_AIRPORT_SUCCESS ] ( state, payload )
+  {
     state.loading = false;
     state.data = payload;
     state.err = null;
   },
-  [types.M_AIRPORT_FAILED](state, payload) {
+  [ types.M_AIRPORT_FAILED ] ( state, payload )
+  {
     state.loading = false;
     state.data = null;
     state.err = payload;
@@ -27,57 +30,72 @@ const mutations = {
 };
 
 const actions = {
-  [types.A_FETCH_LIST_AIRPORT]({ commit }) {
-    commit(types.M_AIRPORT_REQUEST);
+  [ types.A_FETCH_LIST_AIRPORT ] ( { commit } )
+  {
+    commit( types.M_AIRPORT_REQUEST );
     api
-      .get("/airports")
-      .then((result) => {
-        commit(types.M_AIRPORT_SUCCESS, result.data);
-      })
-      .catch((err) => {
-        commit(types.M_AIRPORT_FAILED, err);
-      });
+      .get( "/airports" )
+      .then( ( result ) =>
+      {
+        commit( types.M_AIRPORT_SUCCESS, result.data );
+      } )
+      .catch( ( err ) =>
+      {
+        commit( types.M_AIRPORT_FAILED, err );
+      } );
   },
-  fetchDetailAirport({ commit }, id) {
-    commit(types.M_AIRPORT_REQUEST);
+  fetchDetailAirport ( { commit }, id )
+  {
+    commit( types.M_AIRPORT_REQUEST );
     api
-      .get(`/airports/${id}`)
-      .then((result) => {
-        commit(types.M_AIRPORT_SUCCESS, result.data);
-      })
-      .catch((err) => {
-        commit(types.M_AIRPORT_FAILED, err);
-      });
+      .get( `/airports/${ id }` )
+      .then( ( result ) =>
+      {
+        commit( types.M_AIRPORT_SUCCESS, result.data );
+      } )
+      .catch( ( err ) =>
+      {
+        commit( types.M_AIRPORT_FAILED, err );
+      } );
   },
-  fetchDeleteAirport({ commit, dispatch }, id) {
+  fetchDeleteAirport ( { commit, dispatch }, id )
+  {
     api
-      .delete(`/airports/${id}`)
-      .then(() => {
-        dispatch(types.A_FETCH_LIST_AIRPORT);
-      })
-      .catch((err) => {
-        commit(types.M_AIRPORT_FAILED, err);
-      });
+      .delete( `/airports/${ id }` )
+      .then( () =>
+      {
+        dispatch( types.A_FETCH_LIST_AIRPORT );
+      } )
+      .catch( ( err ) =>
+      {
+        commit( types.M_AIRPORT_FAILED, err );
+      } );
   },
-  fetchUpdateAirport({ commit }, formAirport) {
+  fetchUpdateAirport ( { commit }, formAirport )
+  {
     api
-      .put(`/airports/${formAirport.id}`, formAirport)
-      .then(() => {
-        router.push("/admin/airports");
-      })
-      .catch((err) => {
-        commit(types.M_AIRPORT_FAILED, err);
-      });
+      .put( `/airports/${ formAirport.id }`, formAirport )
+      .then( () =>
+      {
+        router.push( "/admin/airports" );
+      } )
+      .catch( ( err ) =>
+      {
+        commit( types.M_AIRPORT_FAILED, err );
+      } );
   },
-  fetchAddAirport({ commit }, formAirport) {
+  fetchAddAirport ( { commit }, formAirport )
+  {
     api
-      .post(`/airports/`, formAirport)
-      .then(() => {
-        router.push("/admin/airports");
-      })
-      .catch((err) => {
-        commit(types.M_AIRPORT_FAILED, err);
-      });
+      .post( `/airports/`, formAirport )
+      .then( () =>
+      {
+        router.push( "/admin/airports" );
+      } )
+      .catch( ( err ) =>
+      {
+        commit( types.M_AIRPORT_FAILED, err );
+      } );
   },
 };
 
